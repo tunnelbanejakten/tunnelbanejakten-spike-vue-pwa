3b73d75fd87ab8bfdb8eaf4666367463b41c1b66
Mikael Svensson <mikael.svensson@klarna.com>
2021-06-08 22:53:14 +0200

    Show latest captured photo and request HD resolution #7

--- frontend/src/components/Camera.vue
+++ frontend/src/components/Camera.vue
@@ -7,8 +7,9 @@
                         buttonLabel='Status'/>
     <div v-if="showStatus">
       <WebCam
-        width="100%"
-        height="300"
+        :width="videoPreviewDimensions.width"
+        :height="videoPreviewDimensions.height"
+        :resolution="{ width: 1980, height: 1080 }"
         ref="webcam"
         :device-id="deviceId"
         @started="onStarted"
@@ -21,6 +22,15 @@
         <button v-if="!isPlaying" @click="start">Start</button>
         <button v-if="isPlaying" @click="stop">Stop</button>
         <button v-if="isPlaying" @click="capture">Capture</button>
+        <button v-if="!!devices && devices.length > 1" @click="switchCamera">Switch camera</button>
+      </div>
+      <div v-if="!!img">
+        <p>
+          Most recently captured photo:
+        </p>
+        <div>
+          <img :src="img"  class='captured-photo'/>
+        </div>
       </div>
     </div>
   </div>
@@ -30,6 +40,15 @@
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import PrerequisiteStatus, { Status } from '@/components/PrerequisiteStatus.vue'
 import { WebCam } from 'vue-web-cam'
+
+type DeviceData = {
+  deviceId: string
+}
+
+type Dimensions = {
+  width: number,
+  height: number
+}
 
 @Component({
   name: 'Camera',
@@ -42,8 +61,10 @@ export default class Camera extends Vue {
   private img? = ''
   private camera? = ''
   private deviceId? = ''
-  private devices = []
+  private devices: DeviceData[] = []
   private isPlaying = false
+  private videoActualDimensions: Dimensions | null = null
+  private videoPreviewDimensions: Dimensions | null = { width: 300, height: 300 }
 
   toggleStatusShown () {
     this.showStatus = !this.showStatus
@@ -64,10 +85,24 @@ export default class Camera extends Vue {
     }
   }
 
+  @Watch('videoActualDimensions')
+  onVideoActualDimensionsChange (videoActualDimensions: Dimensions) {
+    const { width, height } = videoActualDimensions.width ? videoActualDimensions : { width: 1980, height: 1080 }
+    const ratio = width / height
+    console.log(`📐 Aspect ratio of ${width}x${height} is ${ratio.toFixed(2)}.`)
+    const maxSize = 400
+    if (width > height) {
+      this.videoPreviewDimensions = { width: maxSize, height: maxSize / ratio }
+    } else {
+      this.videoPreviewDimensions = { width: maxSize * ratio, height: maxSize }
+    }
+  }
+
   capture () {
     const cam: any = this.$refs.webcam
     const image = cam.capture()
-    console.log('🏞', image)
+    console.log(`📷 Captured photo. Got ${image.length} bytes.`)
+    this.img = image
     this.prerequisiteStatus = Status.SUCCESS
   }
 
@@ -81,9 +116,20 @@ export default class Camera extends Vue {
     cam.start()
   }
 
-  onStarted (stream: any) {
-    console.log('On Started Event', stream)
+  switchCamera () {
+    const currentIndex = this.devices.findIndex(device => device.deviceId === this.deviceId)
+    this.deviceId = this.devices[(currentIndex + 1) % this.devices.length].deviceId
+  }
+
+  onStarted (mediaStream: any) {
+    console.log('On Started Event', mediaStream)
     this.isPlaying = true
+
+    mediaStream.getVideoTracks().forEach((videoTrack:any) => {
+      const currentSettings = videoTrack.getSettings()
+      console.log(`Got ${currentSettings.width}x${currentSettings.height}`)
+      this.videoActualDimensions = { width: currentSettings.width, height: currentSettings.height }
+    })
   }
 
   onStopped (stream: any) {
@@ -109,5 +155,10 @@ export default class Camera extends Vue {
 </script>
 
 <style scoped>
-
+.captured-photo {
+  border: 10px solid white;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  width: 300px;
+  height: auto;
+}
 </style>
